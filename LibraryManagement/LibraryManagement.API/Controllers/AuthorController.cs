a8d4e0a7052e9e091f9a19ef09928d8df41b944f
Kristaps Kozlovskis <160219564+codekristaps@users.noreply.github.com>
2024-12-11 21:40:05 +0200

    Update AuthorController.cs

--- LibraryManagement/LibraryManagement.API/Controllers/AuthorController.cs
+++ LibraryManagement/LibraryManagement.API/Controllers/AuthorController.cs
@@ -17,6 +17,62 @@ namespace LibraryManagement.API.Controllers
             _dbContext = dbContext;
         }
 
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Author>>> GetAuthors()
+        {
+            return await _dbContext.Authors.ToListAsync();
+        }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Author>> GetAuthor(Guid id)
+        {
+            var author = await _dbContext.Authors.FirstOrDefaultAsync(a => a.AuthorId == id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(author);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> CreateAuthor([FromBody] Author author)
+        {
+            author.AuthorId = Guid.NewGuid();
+            _dbContext.Authors.Add(author);
+            await _dbContext.SaveChangesAsync();
+            return Ok(author);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateAuthor(Guid id, [FromBody] Author author)
+        {
+            if (id != author.AuthorId)
+            {
+                return BadRequest();
+            }
+
+            _dbContext.Update(author);
+            await _dbContext.SaveChangesAsync();
+
+            return Ok(author);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAuthor(Guid id)
+        {
+            var author = await _dbContext.Authors.FindAsync(id);
+
+            if (author == null)
+            {
+                return NotFound();
+            }
+
+            _dbContext.Authors.Remove(author);
+            await _dbContext.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
